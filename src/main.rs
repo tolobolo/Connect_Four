67e9045933b20b9e0cfab539944ca582ca413f52
Emrys <emrys@local>
2025-06-24 19:14:32 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,93 @@
 fn main() {
-    let game = ConnectFour::new();
+    let mut game = ConnectFour::new();
 
     game.round()
 }
+#[derive(Debug, PartialEq)]
+enum Location {
+    Empty,
+    Red,
+    Yellow,
+}
+enum Player {
+    Red,
+    Yellow,
+}
 
 struct ConnectFour {
-    board: Vec<String>,
-    symbol: String
+    board: Vec<Location>,
+    current_player: Player,
+    nb_columns: usize,
+    nb_rows: usize,
 }
 
 impl ConnectFour {
-    fn input(&self, message: &str) -> String {
+    fn new() -> Self {
+        let nb_columns = 7;
+        let nb_rows = 6;
+        let mut board = Vec::new();
+        for _ in 0..nb_rows * nb_columns {
+            board.push(Location::Empty);
+        }
+
+        Self {
+            board,
+            current_player: Player::Red,
+            nb_rows,
+            nb_columns,
+        }
+    }
+
+    fn input(&self, message: &str) -> usize {
         let mut input = String::new();
 
-        loop {
-            println!("{}", message);
-            input.clear();
+        println!("{}", message);
+        input.clear();
 
-            std::io::stdin()
-                .read_line(&mut input)
-                .expect("Failed to read line");
-            input.trim().to_string();
-        }
+        std::io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        return input.trim().parse::<usize>().expect("Failed to read line");
     }
 
-    fn new() -> Self {
-        let mut board: Vec<String> = Vec::new();
-        for i in 0..20 {
-            board.push(i.to_string());
-        }
-
-
-        let symbol = "X".to_string();
-
-        Self {board, symbol}
-    }
-
-    fn choice_symbol(&mut self) {
-        if self.symbol == "X".to_string() {
-            self.symbol = "O".to_string()
-        } else if self.symbol == "O".to_string() {
-            self.symbol = "X".to_string()
-        } else {
-            println!("symbol is wrong {:?}", self.symbol)
+    fn place_tile(&mut self) {
+        let input: usize = self.input("whith row do you want to place in");
+        let coin = match self.current_player {
+            Player::Red => Location::Red,
+            Player::Yellow => Location::Yellow,
+        };
+        for row in (1..=self.nb_rows).rev() {
+            let idx = self.nb_columns * (row - 1) + input - 1;
+            if self.board[idx] == Location::Empty {
+                self.board[idx] = coin;
+                break;
+            }
         }
     }
 
     fn print_board(&self) {
-        for (index, i) in self.board.iter().enumerate() {
-            print!("{} ", i);
-            if (index + 1) % 4 == 0 {
-                println!();
+        for i in 1..self.nb_columns + 1 {
+            print!("{}", i)
+        }
+        println!();
+        for (idx, val) in self.board.iter().enumerate() {
+            match val {
+                Location::Red => print!("X"),
+                Location::Yellow => print!("O"),
+                Location::Empty => print!("."),
+            }
+            if (idx + 1) % self.nb_columns == 0 {
+                println!()
             }
         }
-        println!()
+        println!();
     }
 
-    fn round(&self) {
-        self.print_board()
+    fn round(&mut self) {
+        self.print_board();
+        loop {
+            self.place_tile();
+            self.print_board();
+        }
     }
-
 }
