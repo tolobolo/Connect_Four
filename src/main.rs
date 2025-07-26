abcc824892da4550ea7763141d1e428da16351cc
Emrys <emrys@local>
2025-07-26 11:19:40 +0200

    some changes to make the code testing ready and name changes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 
     game.round()
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Location {
     Empty,
     Red,
@@ -13,6 +13,15 @@ enum Location {
 enum Player {
     Red,
     Yellow,
+}
+
+impl Player {
+    fn as_coin(&self) -> Location {
+        match self {
+            Player::Red => Location::Red,
+            Player::Yellow => Location::Yellow,
+        }
+    }
 }
 
 struct ConnectFour {
@@ -30,6 +39,19 @@ impl ConnectFour {
         for _ in 0..nb_rows * nb_columns {
             board.push(Location::Empty);
         }
+
+        Self {
+            board,
+            current_player: Player::Red,
+            nb_rows,
+            nb_columns,
+        }
+    }
+
+    fn new_with_state(board: Vec<Location>) -> Self {
+        let nb_columns = 7;
+        let nb_rows = 6;
+        let board = board;
 
         Self {
             board,
@@ -100,7 +122,7 @@ impl ConnectFour {
         println!();
     }
 
-    fn choice_symbol(&mut self) {
+    fn set_next_player(&mut self) {
         if self.current_player == Player::Red {
             self.current_player = Player::Yellow
         } else if self.current_player == Player::Yellow {
@@ -110,46 +132,152 @@ impl ConnectFour {
         }
     }
 
-    fn check_four_row(&self) -> bool {
-        let coin = match self.current_player {
-            Player::Red => Location::Red,
-            Player::Yellow => Location::Yellow,
-        };
-        //vannrett
+    fn check_current_player_has_won(&mut self) -> bool {
+        self.has_won(&self.current_player)
+    }
+
+    fn has_won(&self, player: &Player) -> bool {
+        if self.has_won_vertical(&player) {
+            return true;
+        }
+        if self.has_won_horizontal(&player) {
+            return true;
+        }
+        return false;
+    }
+    fn has_won_horizontal(&self, player: &Player) -> bool {
+        let coin = player.as_coin();
         for idx in 0..self.board.len() - 4 {
             if self.board[idx..(idx + 4)].iter().all(|tile| *tile == coin) {
-                println!(" {:?} has won", self.current_player);
+                println!(" {:?} has won", player);
                 return true;
             }
         }
-
-        //loddrett
-        let mut ajecent_coin: usize = 0;
+        false
+    }
+    fn has_won_vertical(&self, player: &Player) -> bool {
+        let coin = player.as_coin();
+        let mut ajecent_coin: usize;
         for idx in 0..=self.board.len() - 1 - 4 * self.nb_columns {
             ajecent_coin = 0;
 
-            //dbg!((idx..=idx + 4 * self.nb_columns).step_by(self.nb_columns));
-            for i in (idx..idx + 4 * self.nb_columns).step_by(self.nb_columns) {
+            dbg!((idx..=idx + 4 * self.nb_columns).step_by(self.nb_columns + 1));
+            for i in (idx..idx + 4 * self.nb_columns).step_by(self.nb_columns + 1) {
                 if self.board[i] == coin {
                     ajecent_coin += 1
                 }
             }
 
             if ajecent_coin == 4 {
-                println!(" {:?} has won", self.current_player);
+                println!(" {:?} has won", player);
                 return true;
             }
         }
-
         false
     }
 
     fn round(&mut self) {
         self.print_board();
-        while !self.check_four_row() {
-            self.choice_symbol();
+        while !self.check_current_player_has_won() {
+            self.set_next_player();
             self.place_tile();
             self.print_board();
         }
+    }
+}
+
+#[cfg(test)]
+#[warn(unused_variables)]
+mod tests {
+    use all_asserts::{assert_false, assert_true};
+
+    use crate::{ConnectFour, Location, Player};
+
+    #[test]
+    fn test_empty() {
+        let e = Location::Empty;
+        let r = Location::Red;
+        let y = Location::Yellow;
+        let cf = ConnectFour::new_with_state(vec![
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+        ]);
+        assert_false!(cf.has_won_vertical(&Player::Red));
+        assert_false!(cf.has_won_horizontal(&Player::Red));
+
+        assert_false!(cf.has_won_horizontal(&Player::Yellow));
+        assert_false!(cf.has_won_horizontal(&Player::Yellow));
+    }
+
+    #[test]
+    #[ignore]
+    fn test_horizontal_top() {
+        let e = Location::Empty;
+        let r = Location::Red;
+        let y = Location::Yellow;
+        let cf = ConnectFour::new_with_state(vec![
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            y, y, y, y, y, e, e, //
+        ]);
+        assert_false!(cf.has_won(&Player::Red));
+        assert_true!(cf.has_won(&Player::Yellow));
+    }
+    #[test]
+    #[ignore]
+    fn test_diagonal() {
+        let e = Location::Empty;
+        let r = Location::Red;
+        let y = Location::Yellow;
+        let cf = ConnectFour::new_with_state(vec![
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, y, e, //
+            e, e, e, e, y, e, e, //
+            e, e, e, y, e, e, e, //
+            e, e, y, y, y, e, e, //
+        ]);
+        assert_false!(cf.has_won(&Player::Red));
+        assert_true!(cf.has_won(&Player::Yellow));
+    }
+    #[test]
+
+    fn test_horizontal_bottom() {
+        let e = Location::Empty;
+        let r = Location::Red;
+        let y = Location::Yellow;
+        let cf = ConnectFour::new_with_state(vec![
+            e, e, e, y, y, y, y, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            e, e, r, e, r, r, r, //
+        ]);
+        assert_false!(cf.has_won_horizontal(&Player::Red));
+        assert_true!(cf.has_won_horizontal(&Player::Yellow));
+    }
+    #[test]
+    fn test_veritcal() {
+        let e = Location::Empty;
+        let r = Location::Red;
+        let y = Location::Yellow;
+        let cf = ConnectFour::new_with_state(vec![
+            e, e, e, e, e, e, e, //
+            e, e, e, e, e, e, e, //
+            y, e, e, e, e, e, e, //
+            y, e, e, e, e, e, e, //
+            y, e, e, e, e, e, e, //
+            y, e, e, e, e, e, e, //
+        ]);
+        assert_false!(cf.has_won_vertical(&Player::Red));
+        assert_true!(cf.has_won_vertical(&Player::Yellow));
     }
 }
